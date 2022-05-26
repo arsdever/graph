f880b52a3953f8b9c91d07303697f3ab11074f9b
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-05-27 01:18:45 +0400

    Use pointers instead of objects

--- include/graph/primitives/graph.hpp
+++ include/graph/primitives/graph.hpp
@@ -7,14 +7,14 @@ namespace graph
 {
     template <is_vertex V,
               is_edge E,
-              is_container vertex_container = std::vector<V>,
-              is_container edge_container = std::vector<E>>
+              is_container vertex_container = std::vector<typename V::ptr_t>,
+              is_container edge_container = std::vector<typename E::ptr_t>>
     class graph
     {
     public:
         graph() { }
 
-        void add_vertex(V v) { std::back_inserter(vertices) = v; }
+        void add_vertex(typename V::ptr_t v) { std::back_inserter(vertices) = v; }
 
     private:
         vertex_container vertices;
