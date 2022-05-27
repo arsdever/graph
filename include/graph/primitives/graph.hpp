12db6600f4dfff11f03a8b9739e945b006e60ec8
Arsen <arsen.gharagyozyn.96@gmail.com>
2022-05-27 19:39:40 +0400

    Refactor graph to use weak pointers instead

--- include/graph/primitives/graph.hpp
+++ include/graph/primitives/graph.hpp
@@ -13,31 +13,56 @@ namespace graph
     class graph
     {
     public:
-        graph() { }
-        void add_vertex(typename V::ptr_t v)
+        typename V::wptr_t create_vertex()
         {
-            if (std::find(vertices.begin(), vertices.end(), v) ==
-                vertices.end())
-                std::back_inserter(vertices) = v;
+            typename V::ptr_t vertex = std::make_shared<V>(vertices.size());
+            add_vertex(vertex);
+            return vertex;
         }
-        void add_edge(typename V::ptr_t v1, typename V::ptr_t v2)
+        typename V::wptr_t add_vertex(typename V::wptr_t vertex)
+        {
+            if (std::find(vertices.begin(), vertices.end(), vertex.lock()) ==
+                vertices.end())
+                std::back_inserter(vertices) = vertex.lock();
+            return vertex;
+        }
+        typename E::wptr_t add_edge(typename V::id_t v1_id,
+                                    typename V::id_t v2_id)
+        {
+            auto v1_it = std::find(vertices.begin(), vertices.end(), v1_id);
+            auto v2_it = std::find(vertices.begin(), vertices.end(), v2_id);
+            if (v1_it == vertices.end() || v2_it == vertices.end())
+                return E::wptr_t();
+            return add_edge(*v1_it, *v2_it);
+        }
+        typename E::wptr_t add_edge(typename V::wptr_t v1,
+                                    typename V::wptr_t v2)
         {
             add_vertex(v1);
             add_vertex(v2);
-            if (std::find_if(edges.begin(),
-                             edges.end(),
-                             [ v1, v2 ](auto e) -> bool
-                             {
+            auto edge_it = std::find_if(edges.begin(),
+                                        edges.end(),
+                                        [ v1, v2 ](auto e) -> bool
+                                        {
                 if constexpr (std::is_base_of<directed_graph_tag,
                                               constraints>::value)
-                    return e->source() == v1 && e->target() == v2;
+                    return e->source().lock() == v1.lock() &&
+                           e->target().lock() == v2.lock();
                 else
-                    return (e->source().lock() == v1 &&
-                            e->target().lock() == v2) ||
-                           (e->source().lock() == v2 &&
-                            e->target().lock() == v1);
-                }) == edges.end())
-                std::back_inserter(edges) = std::make_shared<E>(v1, v2);
+                    return (e->source().lock() == v1.lock() &&
+                            e->target().lock() == v2.lock()) ||
+                           (e->source().lock() == v2.lock() &&
+                            e->target().lock() == v1.lock());
+            });
+            if (edge_it == edges.end())
+            {
+                typename E::ptr_t edge =
+                    std::make_shared<E>(v1.lock(), v2.lock());
+                std::back_inserter(edges) = edge;
+                return edge;
+            }
+
+            return *edge_it;
         }
 
     private:
--- samples/dot_converter.cpp
+++ samples/dot_converter.cpp
@@ -12,12 +12,15 @@ int main(int argc, char** argv)
 
     graph::graph<graph::vertex, graph::edge> graph;
 
-    auto v1 = std::make_shared<graph::vertex>();
-    auto v2 = std::make_shared<graph::vertex>();
-    auto v3 = std::make_shared<graph::vertex>();
+    auto v1 = graph.create_vertex();
+    auto v2 = graph.create_vertex();
+    auto v3 = graph.create_vertex();
 
-    graph.add_vertex(v1);
-    graph.add_edge(v2, v3);
+    auto e1 = graph.add_edge(v2, v3);
+    auto e2 = graph.add_edge(v3, v3);
+
+    std::cout << e1.lock()->is_loop() << std::endl;
+    std::cout << e2.lock()->is_loop() << std::endl;
 
     return 0;
 }
