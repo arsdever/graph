8c27bef74050e833784318361f79440c8144bbf2
Arsen <arsen.gharagyozyn.96@gmail.com>
2022-05-27 19:41:38 +0400

    Add verification for existing node addition

--- samples/dot_converter.cpp
+++ samples/dot_converter.cpp
@@ -17,10 +17,12 @@ int main(int argc, char** argv)
     auto v3 = graph.create_vertex();
 
     auto e1 = graph.add_edge(v2, v3);
-    auto e2 = graph.add_edge(v3, v3);
+    auto e2 = graph.add_edge(v2, v3);
+    auto e3 = graph.add_edge(v3, v3);
 
     std::cout << e1.lock()->is_loop() << std::endl;
-    std::cout << e2.lock()->is_loop() << std::endl;
+    std::cout << (e2.lock() == e1.lock()) << std::endl;
+    std::cout << e3.lock()->is_loop() << std::endl;
 
     return 0;
 }
